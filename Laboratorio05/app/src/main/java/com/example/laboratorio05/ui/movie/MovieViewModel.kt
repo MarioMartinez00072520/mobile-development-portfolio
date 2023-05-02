7c9a47d8d29e5797290fffc9b1b5bed61c155ff7
MarioMartinez00072520 <00072520@uca.edu.sv>
2023-05-02 14:41:22 -0600

    Add: backend functionality laboratorio07

--- Laboratorio05/app/src/main/java/com/example/laboratorio05/ui/movie/MovieViewModel.kt
+++ Laboratorio05/app/src/main/java/com/example/laboratorio05/ui/movie/MovieViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.laboratorio05.ui.movie
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
 import androidx.lifecycle.viewmodel.initializer
@@ -9,10 +10,56 @@ import com.example.laboratorio05.data.model.MovieModel
 import com.example.laboratorio05.repositories.MovieRepository
 
 class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
+
+    var title = MutableLiveData("")
+    var category = MutableLiveData("")
+    var description = MutableLiveData("")
+    var qualification = MutableLiveData("")
+    var status = MutableLiveData("")
+
     fun getMovies() = repository.getMovies()
 
     fun addMovies(movie: MovieModel) = repository.addMovies(movie)
 
+    fun createMovie() {
+        if (!validateData()) {
+            status.value = WRONG_INFORMATION
+            return
+        }
+
+        val movie = MovieModel(
+            title.value!!,
+            category.value!!,
+            description.value!!,
+            qualification.value!!,
+        )
+
+        addMovies(movie)
+        clearData()
+
+        status.value = MOVIE_CREATED
+    }
+
+    private fun validateData(): Boolean{
+        when{
+            title.value.isNullOrEmpty() -> return false
+            category.value.isNullOrEmpty() -> return false
+            description.value.isNullOrEmpty() -> return false
+            qualification.value.isNullOrEmpty() -> return false
+        }
+        return true
+    }
+
+    private fun clearData(){
+        title.value = ""
+        category.value = ""
+        description.value = ""
+        qualification.value = ""
+    }
+
+    private fun clearStatus(){
+        status.value = INACTIVE
+    }
     companion object {
         val Factory = viewModelFactory {
             initializer {
@@ -20,5 +67,12 @@ class MovieViewModel(private val repository: MovieRepository) : ViewModel() {
                 MovieViewModel(app.movieRepository)
             }
         }
+
+        const val MOVIE_CREATED = "Movie Created"
+        const val WRONG_INFORMATION = "Wrong Information"
+        const val INACTIVE = ""
+
     }
+
+
 }
--- Laboratorio05/app/src/main/java/com/example/laboratorio05/ui/movie/NewMovieFragment.kt
+++ Laboratorio05/app/src/main/java/com/example/laboratorio05/ui/movie/NewMovieFragment.kt
@@ -14,14 +14,18 @@ import com.example.laboratorio05.R
 import com.example.laboratorio05.data.category2
 import com.example.laboratorio05.data.model.MovieModel
 import com.example.laboratorio05.data.qualification2
+import com.example.laboratorio05.databinding.FragmentNewMovieBinding
 import kotlin.math.log
 
 
 class NewMovieFragment : Fragment() {
 
+
     private val movieViewModel: MovieViewModel by activityViewModels {
         MovieViewModel.Factory
     }
+
+    private lateinit var binding: FragmentNewMovieBinding
 
     private lateinit var nameEditText: EditText
     private lateinit var categoryEditText: EditText
@@ -32,9 +36,9 @@ class NewMovieFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_new_movie, container, false)
+    ): View {
+        binding = FragmentNewMovieBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -52,6 +56,10 @@ class NewMovieFragment : Fragment() {
         }
 
 
+    }
+
+    private fun setViewModel(){
+        binding.viewmodel = movieViewModel
     }
 
     fun bind(){
